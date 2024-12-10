a6d06a7b0846d56b06d49d755b11e7214a4ab79e
Ahmed Yousri <ayousry42@gmail.com>
2024-12-11 00:07:55 +0200

    Adding products by admin

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
-
-const Login = () => {
+import axios from 'axios'
+import {backendUrl} from '../App'
+import { toast } from 'react-toastify';
+const Login = ({setToken}) => {
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
     const onSubmitHandler = async(e)=>{
         try {
             e.preventDefault();
-            console.log(email,password);
+            // console.log(email,password);
+            const response = await axios.post(backendUrl + '/api/user/admin',{email,password});
+            if(response.data.success){
+                setToken(response.data.token)
+            }
+            else{
+                toast.error(response.data.message)
+            }
             
         } catch (error) {
-            
+            console.log(error);
+            toast.error(error.message)
         }
     }
     return (
+        
         <div className='min-h-screen flex items-center justify-center w-full'>
             <div className="bg-white shadow-md rounded-lg px-8 py-6 max-w-md">
                 <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
